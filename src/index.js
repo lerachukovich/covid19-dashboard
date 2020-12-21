06f17e113495f02acba9ca1b9682a21c383c20e3
Lera Chukovich <chukalera@gmail.com>
2020-12-21 13:16:28 +0100

    feat: add configuration to a project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './styles/style.less';
+import GlobalCases from "./GlobalCases";
+import CasesByCountry from "./CasesByCountry";
+import Map from "./Map";
+
+window.addEventListener('load', function () {
+    let globalCases = new GlobalCases();
+    let casesByCountry = new CasesByCountry();
+    casesByCountry.getData();
+    let map = new Map();
+    document.body.appendChild(globalCases.render());
+    document.body.appendChild(casesByCountry.render());
+    document.body.appendChild(map.render());
+});
+
