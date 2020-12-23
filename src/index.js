b20d84cafd787c45c8c434d33a1c0957aac5fc41
Lera Chukovich <chukalera@gmail.com>
2020-12-23 16:34:38 +0100

    feat: add styles to table

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './styles/style.less';
 import GlobalCases from "./GlobalCases";
-import CasesByCountry from "./CasesByCountry";
+import CasesList from "./CasesList";
 import Map from "./Map";
 import DeathByCountry from "./DeathByCountry";
 import RecoveredByCountry from "./RecoveredByCountry";
+import Table from "./Table";
 
 const leftContainer = document.createElement("div");
 const rightContainer = document.createElement("div");
@@ -12,18 +13,21 @@ rightContainer.classList.add("right__container");
 
 window.addEventListener('load', function () {
     let globalCases = new GlobalCases();
-    let casesByCountry = new CasesByCountry();
+    let casesByCountry = new CasesList();
     casesByCountry.getData();
     let map = new Map();
     let deathByCountry = new DeathByCountry();
     let recoveredByCountry = new RecoveredByCountry();
+    let table = new Table();
+    table.getData();
 
     document.body.appendChild(leftContainer);
     leftContainer.appendChild(globalCases.render());
     leftContainer.appendChild(casesByCountry.render());
     document.body.appendChild(map.render());
     document.body.appendChild(rightContainer);
-    rightContainer.appendChild(deathByCountry.render());
-    rightContainer.appendChild(recoveredByCountry.render());
+    // rightContainer.appendChild(deathByCountry.render());
+    // rightContainer.appendChild(recoveredByCountry.render());
+    rightContainer.appendChild(table.render());
 });
 
